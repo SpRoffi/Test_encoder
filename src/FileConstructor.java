aff76a9715976f4e806eee2686a15417af3b85d3
SpRoffi <sersemsed@gmail.com>
2025-04-07 18:55:56 +0300

    added modern File servis

--- src/FileConstructor.java
+++ src/FileConstructor.java
@@ -1,13 +1,11 @@
 import java.nio.file.Path;
 
 public class FileConstructor {
-    public Path newFileName;
 
     public Path FileName(Path fileName, String text){
 
         var words = String.valueOf(fileName.getFileName()).split("\\.");
-        newFileName = fileName.getParent().resolve(Path.of(words[0] + text +"." + words[1]));
 
-        return newFileName;
+        return fileName.getParent().resolve(Path.of(words[0] + text +"." + words[1]));
     }
 }
